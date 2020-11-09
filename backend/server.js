3bf62d504a426a0b2722ad0de2cd1f799056f7bf
Vital Pro <vitalijprokofjev@vitalijs-mbp.home>
2020-11-09 16:49:07 +0000

    publish

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,11 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server running on", process.env.PORT || 5000);
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+});
+
+app.listen(config.PORT, () => {
+  console.log('Server started at http://localhost:5000');
 });
